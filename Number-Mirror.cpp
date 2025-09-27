68637ce2ebb3971a7273908d3ed79f4eb61f8ffb
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-27 21:19:46 +0600

    solution Done

---
+++ Number-Mirror.cpp
@@ -0,0 +1,17 @@
+// ================================================
+//                Platform : CodeChef
+//                Contest  : Practice (Beginner)
+//                Problem  : Number Mirror
+//                Language : C++
+//                Topic    : [Number Mirror]
+//                File     : Number Mirror.cpp
+// ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a;
+    return 0;
+}
